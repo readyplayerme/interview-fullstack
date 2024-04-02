b5935582901bfc928434548be16506e4ca4899d0
MaxAndreassenRPM <max@wolf3d.io>
2024-04-02 13:54:47 +0100

    fix: starts off with one stale avatar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,9 @@ function App() {
     <div className="App">
       <div>
           <h1>Your avatar:</h1>
-          Your avatar_id <input type="text"/> <br/> (examples: 6002118ef4c903f8d1052305, 600ab7eb000eff7eb8b01eb2)
+          Your avatar_id <input type="text"/> <br/> (examples: 6002118ef4c903f8d1052304, 600ab7eb000eff7eb8b01eb2)
             <a href="https://reactjs.org" target="_blank">
-              <img src="http://localhost:3000/?id=6002118ef4c903f8d1052305" className="logo react" />
+              <img src="http://localhost:3000/?id=6002118ef4c903f8d1052304" className="logo react" />
             </a>
       </div>
      
