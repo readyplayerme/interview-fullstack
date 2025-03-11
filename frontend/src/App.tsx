be1c287731e90b45e145fe9edb7c58f99b4e6798
Kristo Karp <kristokarp@gmail.com>
2025-03-11 09:40:34 +0200

    fix: update avatar ids

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
-
 function App() {
   return (
     <div className="App">
       <div>
-          <h1>Your avatar:</h1>
-          Your avatar_id <input type="text"/> <br/> (examples: 6002118ef4c903f8d1052304, 600ab7eb000eff7eb8b01eb2)
-            <a href="https://reactjs.org" target="_blank">
-              <img src="http://localhost:3000/?id=6002118ef4c903f8d1052304" className="logo react" />
-            </a>
+        <h1>Your avatar:</h1>
+        Your avatar_id <input type="text" /> <br /> (examples:
+        67cfe88456c84ecbd8f88d73, 67cfe7d6c9fac4e615caf701)
+        <img
+          src="http://localhost:3000/?id=6002118ef4c903f8d1052304"
+          className="logo react"
+        />
       </div>
-     
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
